4e7b32c2b9a450b327893dae2c21e89bd8e5dc53
Lucas Gandolfi <lucas_gandolfi@terra.com.br>
2018-08-24 17:35:39 -0300

    combat imprv
    life is hard

--- Assets/Scripts/Combat.cs
+++ Assets/Scripts/Combat.cs
@@ -1,28 +1,63 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class Combat : MonoBehaviour {
 	
-	ActionList ac;
+		ActionList ac;
+    public int myComboCost;
+    public int myComboAtk;
+    public int myComboMov;
+    public int myComboTreat;
+    public int myComboComplexity;
+    public bool staEnough;
 
-	public List<Action> mycombo = new List<Action>();
+    public List<Action> mycombo = new List<Action>();
 
-// name, hp, sta, def, dodge, dex
-	public Player player = new Player("Bobz", 15, 9, 3, 3, 3);
+    public void MyComboClear()
+    {
+        myComboCost = 0;
+        myComboAtk = 0;
+        myComboMov = 0;
+        myComboTreat = 0;
+        myComboComplexity = 0;
+        mycombo.Clear();
+    }
+    public bool StaCheck()
+    {
+        if(player.staNow > myComboCost)
+        {
+            staEnough = true;
+        }
+        if(player.staNow < myComboCost)
+        {
+            staEnough = false;
+        }
+        return staEnough;
+    }
 
-	public void MyComboLog(){
+    // name, hp, sta, def, dodge, dex
+    public Player player = new Player("Bobz", 15, 9, 3, 3, 3);
+
+    public int MyComboSum()
+    {
+        foreach (Action action in mycombo)
+        {
+            myComboCost += action.cost;
+            myComboAtk += action.atk;
+            myComboMov += action.mov;
+            myComboTreat += action.treat;
+            myComboComplexity += action.complexity;
+
+        }
+        return myComboCost;
+    }
+
+    public void MyComboLog(){
 		Debug.Log("Actions in mycombo list:");
 			foreach (Action action in mycombo) {
 				Debug.Log("Name: " + action.name + " Atk: " + action.atk + " Cost: " + action.cost);}}
-
-	public ComboCostSum(){
-  	public int comboCost;
-  	foreach(Action action in mycombo)
-  	{
-  		comboCost += action.cost;
-  	}
-  	return comboCost;} 
 
 	public void Inputs(){
 		if (Input.GetKeyDown(KeyCode.Q))
@@ -60,7 +95,7 @@ public class Combat : MonoBehaviour {
 		{
 			var lastcombo = new List<Action>(mycombo);
 			Debug.Log("Space - combo confirm");
-			mycombo.Clear();
+            MyComboClear();
 		}}
 	
 	public void PlayerLog(){}
